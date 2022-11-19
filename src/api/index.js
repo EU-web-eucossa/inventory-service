3dee52a2088136521f8caae57e61eaa2b3c5c651
Felix Orinda <forinda82@gmail.com>
2022-11-19 05:34:35 +0300

    Add error handler and 404 resource

--- src/api/index.js
+++ src/api/index.js
@@ -3,19 +3,21 @@
  * @ Author: Felix Orinda
  * @ Create Time: 2022-11-18 10:54:18
  * @ Modified by: Felix Orinda
- * @ Modified time: 2022-11-19 05:12:36
+ * @ Modified time: 2022-11-19 05:31:50
  * @ Description:
  */
 const swaggerDocs = require('swagger-jsdoc');
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const SwaggerUIExpress = require('swagger-ui-express');
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 require('./run');
 const { baseLogger } = require('../utils/logger');
 const productRoutes = require('./routes/products.routes');
 const categoryRoutes = require('./routes/categories.routes');
 const { apiAddress, apiPort } = require('../config');
+const errorhandler = require('../errors/errorhandler');
 
 const swaggerOptions = {
 	definition: {
@@ -26,8 +28,12 @@ const swaggerOptions = {
 		},
 		servers: [
 			{
-				url: `http://${apiAddress}`,
+				url: `http://${apiAddress}/api/v1`,
+
 			},
+			{
+				url:'https://eucossa-bc.herokuapp.com/api/v1',
+			}
 		],
 	},
 	apis: ['./routes/products.routes.js', './routes/categories.routes.js'], // files containing swagger annotations as above
@@ -44,11 +50,24 @@ const api = ({ app } = {}) => {
 	router.use('/categories', categoryRoutes);
 	router.use('/products', productRoutes);
 	app.use('/api/v1', router);
-	app.use('/docs', SwaggerUIExpress.serve, SwaggerUIExpress.setup(openApiDocs));
+	app.use(
+		'/docs',
+		SwaggerUIExpress.serve,
+		SwaggerUIExpress.setup(openApiDocs),
+	);
+	app.all('*', (req, res) => {
+		res.status(404).json({
+			status: 'error',
+			message: 'Route not found',
+			path: req.originalUrl,
+		});
+	});
+	app.use(errorhandler);
 };
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 api({ app });
 
